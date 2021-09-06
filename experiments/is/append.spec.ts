1e18303251dcd5fb8b7eac541c0acc992ec9a6ed
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2021-09-06 20:24:13 -0300

    ✅ (is): create unit tests infrastructure

--- experiments/is/append.spec.ts
+++ experiments/is/append.spec.ts
@@ -1,5 +1,19 @@
-import append from './append';
+import append from './append.js';
 
 test('returns new array', () => {
+  const array = [1, 2, 3, 4];
 
+  expect(append(array, 5)).not.toBe(array);
+});
+
+test('appends value at the end of new array', () => {
+  const array = [1, 2, 3, 4];
+
+  expect(append(array, 5)).toEqual([1, 2, 3, 4, 5]);
+});
+
+test("doesn't concat when receives another array as argument", () => {
+  const array = [1, 2, 3, 4];
+
+  expect(append(array, [5])).toEqual([1, 2, 3, 4, [5]]);
 });
--- experiments/is/append.ts
+++ experiments/is/append.ts
@@ -1,5 +1,5 @@
-function append<T, U>(array: readonly T[], value: U): readonly (T | U)[];
 function append<T>(array: readonly T[], value: T): readonly T[];
+function append<T, U>(array: readonly T[], value: U): readonly (T | U)[];
 function append(array: readonly unknown[], value: unknown): readonly unknown[] {
   return array.concat([value]);
 }
